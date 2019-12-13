918373879de2f2e69f93a27426f7875a81c098db
julien_calvignac <j.calvignac@laposte.net>
2019-12-13 10:33:06 +0100

    version commentée

--- dark-place-game/CurrencyHolder.cs
+++ dark-place-game/CurrencyHolder.cs
@@ -20,6 +20,9 @@ namespace dark_place_game
         // Le champs interne de la property
         private string currencyName = CURRENCY_DEFAULT_NAME;
         /// Le montant actuel
+
+        //Getter et setter
+        //**Principe à étudier
         public int CurrentAmount
         {
             get { return currentAmount; }
@@ -41,50 +44,63 @@ namespace dark_place_game
         }
         // Le champs interne de la property
         private int capacity = 0;
+        //Constructeur de la classe CurrencyHolder
         public CurrencyHolder(string name, int capacity, int amount)
         {
-
+            //Principe de base du constructeur, liant les valeurs données en attribut avec les variables associées
+            //**Principe à étudier
             Capacity = capacity;
             CurrencyName = name;
             CurrentAmount = amount;
 
+            //Définition des exceptions permettant d'éviter les situations empêchant le programme de fonctionner correctement
+            //-Repère si le montant "essai" de dépasser le minimum valide
             if (amount < 0) { throw new System.ArgumentException("Parameter cannot be null", "original"); }
+            //-Repère si le nom est bien renseigné
             if (name == null){throw new System.ArgumentException("Parameter cannot be null", "original");}
             if (name == ""){throw new System.ArgumentException("Parameter cannot be null", "original");}
+            //-Repère si le nom a une taille valide
             if (name.Length < 4) { throw new System.ArgumentException("Parameter cannot be null", "original"); }
             if (name.Length > 10) { throw new System.ArgumentException("Parameter cannot be null", "original"); }
+            //-Repère si le nom commence par la lettre a(maj ou min, aucun n'est accepté)
             if (name.Substring(0, 1) == "A") { throw new System.ArgumentException("Parameter cannot be null", "original"); }
             if (name.Substring(0, 1) == "a") { throw new System.ArgumentException("Parameter cannot be null", "original"); }
+            //-Repère si la capacité a une valeur valide
             if (capacity <= 0) { throw new System.ArgumentException("Parameter cannot be null", "original"); }
         }
 
 
-
+        //Méthode déterminant si la bourse est vide ou non
         public bool IsEmpty(int amount)
-        {
+        {   //Renvoie un true si le montant contenu dans la bourse est à 0
+            //Peut-être à modifier le == par <=
             if (amount == 0){return true;}
             else return false;
         }
+
+        //Méthode déterminant si la bourse est pleine ou non
         public bool IsFull(int capacity, int amount, int optionalAddAmount = 0)
-        {
-            if (capacity == amount){return true;}
+        {   //Renvoie true si la bourse est pleine ou déborde, ou renvoie false autrement
+            if (capacity == amount){return true;}   
             else if (capacity <= (amount + optionalAddAmount)){return true;}
             else return false;
         }
+        //Méthode permettant d'augmenter le montant contenu dans la bourse
         public int Put(int amount, int addAmount)
-        {
+        {   //Le montant ainsi que sa modification ne peuvent être de valeur négative,
+            //Relève les exceptions si ces montants sont inférieurs ou égaux à 0
             if (addAmount <= 0) { throw new System.ArgumentException("Parameter cannot be null", "original"); }
             if (amount <= 0) { throw new System.ArgumentException("Parameter cannot be null", "original"); }
-            //if ((amount + add_amount) > capacity){throw new System.ArgumentException("Parameter cannot be null", "original");}
-            amount = amount + addAmount;
+            amount = amount + addAmount;//Renvoie la valeur amount incrémentée par la valeur demandée
             return (amount);
         }
+        //Méthode permettant de diminuer le montant contenu dans la bourse
         public int Withdraw(int amount, int removeAmount)
-        {
+        {   //Même principe que la méthode Put
             if (removeAmount <= 0) { throw new System.ArgumentException("Parameter cannot be null", "original"); }
             if (amount <= 0) { throw new System.ArgumentException("Parameter cannot be null", "original"); }
 
-            amount = amount - removeAmount;
+            amount = amount - removeAmount;//Renvoie la valeur amount décrémentée par la valeur demandée
             return amount;
         }
 
